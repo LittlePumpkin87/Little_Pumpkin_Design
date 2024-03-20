09ec6e41d6428d14141d0d1671b13d798c251090
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2024-03-20 21:10:47 +0100

    changed the Spam protection and added another echo page for spam bots

--- PHP/form-handler.php
+++ PHP/form-handler.php
@@ -1,43 +1,20 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-function honeypot_validade($req)
-{
-  if (!empty ($req)) {
-    $honeypot_fields = [
-      "phone",
-      "email",
-      "name"
-    ];
-    foreach ($honeypot_fields as $field) {
-      if (isset ($req[$field]) && !empty ($req[$field])) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-if (honeypot_validade($_REQUEST)) {
-  $is_spammer = false;
-} else {
-  $is_spammer = true;
-}
-if (isset ($_POST['buttonpoyx'])) {
-  $to = "orders@little-pumpkin-design.com";
-  $from = $_POST['emailpoyx'];
-  $vnamepoyx = $_POST['vnamepoyx'];
-  $znamepoyx = $_POST['znamepoyx'];
-  $phonepoyx = $_POST['phonepoyx'];
-  $subjectpoyx = "Anfrage Kontaktformular";
-  $subjectpoyx2 = "Kopie deiner Nachricht";
-  $messagepoyx = $vnamepoyx . " " . $znamepoyx . "\n\n" . $phonepoyx . "\n\n" . "schreibt folgendes:" . "\n\n" . $_POST['messagepoyx'];
-  $messagepoyx2 = "Hallo $vnamepoyx . Hier ist eine Kopie deiner Nachricht " . "\n\n" . $_POST['messagepoyx'];
-  $headerspoyx = "From:" . $from;
-  $headerspoyx2 = "From:" . $to;
-  mail($to, $subjectpoyx, $messagepoyx, $headerspoyx);
-  mail($from, $subjectpoyx2, $messagepoyx2, $headerspoyx2);
-  echo '<meta http-equiv="refresh" content="0;url=https://little-pumpkin-design.com/thank_you.html" />';
+if (($form['ohnohoney']) == 'spambot') {
+    print("No Bots allowed here!");
+} else if (isset($_POST['buttonpoyx'])) {
+    $to = "orders@little-pumpkin-design.com";
+    $from = $_POST['emailpoyx'];
+    $vnamepoyx = $_POST['vnamepoyx'];
+    $znamepoyx = $_POST['znamepoyx'];
+    $phonepoyx = $_POST['phonepoyx'];
+    $subjectpoyx = "Anfrage Kontaktformular";
+    $subjectpoyx2 = "Kopie deiner Nachricht";
+    $messagepoyx = $vnamepoyx . " " . $znamepoyx . "\n\n" . $phonepoyx . "\n\n" . "schreibt folgendes:" . "\n\n" . $_POST['messagepoyx'];
+    $messagepoyx2 = "Hallo $vnamepoyx . Hier ist eine Kopie deiner Nachricht " . "\n\n" . $_POST['messagepoyx'];
+    $headerspoyx = "From:" . $from;
+    $headerspoyx2 = "From:" . $to;
+    mail($to, $subjectpoyx, $messagepoyx, $headerspoyx);
+    mail($from, $subjectpoyx2, $messagepoyx2, $headerspoyx2);
+    echo '<meta http-equiv="refresh" content="0;url=https://little-pumpkin-design.com/thank_you.html" />';
 }
 ?>
