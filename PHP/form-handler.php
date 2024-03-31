5f914cbfeb1eea173364475d4c8d4125479a1816
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2024-03-31 14:36:30 +0200

    .

--- PHP/form-handler.php
+++ PHP/form-handler.php
@@ -10,7 +10,12 @@ if (isset ($_POST['button'])) {
   $message = $vname . " " . $zname . "\n\n" . $phone . "\n\n" . "schreibt folgendes:" . "\n\n" . $_POST['message'];
   $message2 = "Hallo $vname, hier ist eine Kopie deiner Nachricht " . "\n\n" . $_POST['message'];
   $headers = "From:" . $from;
+  $headers .= "\r\nReply-To: " . $from; // Add Reply-To header to set reply address
   $headers2 = "From:" . $to;
+
+  // Additional headers to comply with DMARC policy
+  $headers .= "\r\nMIME-Version: 1.0";
+  $headers .= "\r\nContent-Type: text/plain; charset=utf-8";
   mail($to, $subject, $message, $headers);
   mail($from, $subject2, $message2, $headers2);
   echo '<meta http-equiv="refresh" content="0;url=https://little-pumpkin-design.com/thank_you.html" />';
